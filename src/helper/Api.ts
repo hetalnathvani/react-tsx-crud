214cd9c1678495aeedc03b0a656726ecf6a9a237
hetalnathvani <nathvanihetal@gmail.com>
2024-10-10 14:19:19 +0530

    Edit and Delete API general configurational changes

--- src/helper/Api.ts
+++ src/helper/Api.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import config from "../config";
 
 interface Params {
   baseUrl: string;
@@ -18,11 +17,29 @@ const getConfig: Params = {
 
 // Config for post method based API calls
 const postConfig: Params = {
-  baseUrl: config.REACT_PUBLIC_API_PATH,
+  baseUrl: "http://127.0.0.1:3030/api/v1",
   headers: {
     Authorization: "",
   },
   method: "post",
+};
+
+// Config for put method based API calls
+const putConfig: Params = {
+  baseUrl: "http://127.0.0.1:3030/api/v1",
+  headers: {
+    Authorization: "",
+  },
+  method: "PUT",
+};
+
+// Config for delete method based API calls
+const deleteConfig: Params = {
+  baseUrl: "http://127.0.0.1:3030/api/v1",
+  headers: {
+    Authorization: "",
+  },
+  method: "DELETE",
 };
 
 export const getAPI = async (url: string, data: any): Promise<any> => {
@@ -50,6 +67,47 @@ export const postAPI = async (url: string, data: any): Promise<any> => {
     ...postConfig,
     url: `${getConfig.baseUrl}/${url}`,
     data,
+  })
+    .then((response) => {
+      return {
+        status: response.status,
+        data: response.data,
+      };
+    })
+    .catch((error) => {
+      console.log(error);
+      return {
+        status: error.status,
+        data: error.response,
+      };
+    });
+};
+
+export const putAPI = async (url: string, data: any): Promise<any> => {
+  return await axios({
+    ...putConfig,
+    url: `${putConfig.baseUrl}/${url}`,
+    data,
+  })
+    .then((response) => {
+      return {
+        status: response.status,
+        data: response.data,
+      };
+    })
+    .catch((error) => {
+      console.log(error);
+      return {
+        status: error.status,
+        data: error.response,
+      };
+    });
+};
+
+export const deleteAPI = async (url: string): Promise<any> => {
+  return await axios({
+    ...deleteConfig,
+    url: `${deleteConfig.baseUrl}/${url}`,
   })
     .then((response) => {
       return {
