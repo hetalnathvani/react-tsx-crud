5807c8ddaa361894b30af206f1230e418e53e0e5
hetalnathvani <nathvanihetal@gmail.com>
2024-10-14 12:14:07 +0530

    Project list basic design changes |WIP

--- src/view/projects/index.tsx
+++ src/view/projects/index.tsx
@@ -1,7 +1,92 @@
-export const ProjectList = () => {
+import {
+  Avatar,
+  Box,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { pink } from "@mui/material/colors";
+// import AddEmployee from "./add";
+// import { DeleteEmployee } from "./delete";
+// import { EditEmployee } from "./edit";
+import { useEffect, useState } from "react";
+import { getAPI } from "../../helper/Api";
+
+interface EnhanceTableHeadProps {}
+
+function EnhancedTableHead(props: EnhanceTableHeadProps) {
   return (
-    <div>
-      <h1>Projects</h1>
-    </div>
+    <TableHead>
+      <TableRow>
+        <TableCell>ID</TableCell>
+        <TableCell>Image</TableCell>
+        <TableCell>Name</TableCell>
+        <TableCell>Email</TableCell>
+        <TableCell>Project</TableCell>
+        <TableCell>City</TableCell>
+        <TableCell>Education</TableCell>
+        <TableCell>Action</TableCell>
+      </TableRow>
+    </TableHead>
+  );
+}
+
+export const ProjectList = () => {
+  const [data, setData] = useState<any[]>([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    getAPI("projects", "").then((res) => {
+      setData(res.data.data);
+    });
+  };
+
+  return (
+    <Box sx={{ paddingLeft: "64px" }}>
+      <Box sx={{ ml: 4, display: "flex", justifyContent: "space-between" }}>
+        <Typography variant="h6">Projects listing</Typography>
+        {/* <AddEmployee /> */}
+      </Box>
+      <Paper sx={{ width: "100%", mb: 2 }}>
+        <TableContainer>
+          <Table
+            sx={{ minWidth: 750 }}
+            aria-labelledby="tableTitle"
+            size={"medium"}
+          >
+            <EnhancedTableHead />
+            <TableBody>
+              {/* {data?.map((r, index) => (
+                <TableRow key={index}>
+                  <TableCell>{index + 1}</TableCell>
+                  <TableCell>
+                    <Avatar sx={{ bgcolor: pink[500] }}>
+                      {r.name.charAt(0)}
+                    </Avatar>
+                  </TableCell>
+                  <TableCell>{r.name}</TableCell>
+                  <TableCell>{r.email}</TableCell>
+                  <TableCell>{r.projects}</TableCell>
+                  <TableCell>{r.city}</TableCell>
+                  <TableCell>{r.education}</TableCell>
+                  <TableCell sx={{ display: "flex" }}>
+                    <EditEmployee record={r} />
+                    <DeleteEmployee id={r._id} />
+                  </TableCell>
+                </TableRow>
+              ))} */}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+    </Box>
   );
 };
